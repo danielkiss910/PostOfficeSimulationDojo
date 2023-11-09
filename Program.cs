a625184b79f89357b36cc14709e53e22c686f0fc
Daniel Kiss <danielkiss910@gmail.com>
2023-11-09 11:22:09 +0100

    Add service folder class, fill main method to test on console

--- Program.cs
+++ Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using PostOfficeSimulationDojo.Model;
+using PostOfficeSimulationDojo.Service;
+
+namespace PostOfficeSimulationDojo;
+
+public static class Program
+{
+    static void Main()
+    {
+        var postOffice = new PostOffice();
+        
+        postOffice.PostItem(new Letter
+        {
+            Address = "1 Vaci ut",
+            PostDate = DateTime.Today
+        });
+        
+        postOffice.PostItem(new SmallBox
+        {
+            Address = "2 Vaci ut",
+            PostDate = DateTime.Today,
+            WeightInGrams = 500
+        });
+        
+        postOffice.PostItem(new BigBox
+        {
+            Address = "3 Vaci ut",
+            PostDate = DateTime.Today,
+            WeightInGrams = 2000
+        });
+
+        var todaysItems = postOffice.GetItemsPostedOn(DateTime.Today);
+
+        var totalIncomeFromLetters = postOffice.CalculateTotalIncomeByType<Letter>();
+        var totalIncomeFromSmallBoxes = postOffice.CalculateTotalIncomeByType<SmallBox>();
+        var totalIncomeFromBigBoxes = postOffice.CalculateTotalIncomeByType<BigBox>();
+
+        var totalIncome = totalIncomeFromLetters + totalIncomeFromSmallBoxes + totalIncomeFromBigBoxes;
+        
+        Console.WriteLine($"Posted items amount today: {todaysItems.Count()}");
+        
+        Console.WriteLine($"Total income from letters: {totalIncomeFromLetters} euros");
+        Console.WriteLine($"Total income from small boxes: {totalIncomeFromSmallBoxes} euros");
+        Console.WriteLine($"Total income from big boxes: {totalIncomeFromBigBoxes} euros");
+        Console.WriteLine($"Total income: {totalIncome} euros");
+    }
+}
--- Service/PostOffice.cs
+++ Service/PostOffice.cs
@@ -1,6 +1,16 @@
+using PostOfficeSimulationDojo.Model;
+
 namespace PostOfficeSimulationDojo.Service;
 
 public class PostOffice
 {
-    
+    private List<PostedItem> _postedItems = new List<PostedItem>();
+
+    public void PostItem(PostedItem item) => _postedItems.Add(item);
+
+    public IEnumerable<PostedItem> GetItemsPostedOn(DateTime date)
+        => _postedItems.Where(item => item.PostDate.Date == date.Date);
+
+    public decimal CalculateTotalIncomeByType<T>() where T : PostedItem
+        => _postedItems.OfType<T>().Sum(item => item.Price);
 }
